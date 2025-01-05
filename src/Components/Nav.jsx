772c5a4f0a3a77e3f8d3d2236f59c67b5747414a
syedsufiyan888 <syedsufiyan4567@gmail.com>
2025-01-05 22:12:07 +0530

    updated

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,3 +1,9 @@
-export default function Nav(){
-    return "helo"
+
+const Nav = () => {
+    return (
+    <div className="bg-blue-600">
+        <h2 className="">Hola</h2>
+    </div>
+    )
 }
+export default Nav;
